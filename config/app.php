ca7315764e254e7466f967d4c4a13c99d76e9d01
charlesphandurand <charles.phandurand@gmail.com>
2024-04-24 09:34:13 +0800

    konfigurasi dan instalasi yajra datatables

--- config/app.php
+++ config/app.php
@@ -175,6 +175,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // yajra datatables -----
+        Yajra\DataTables\DataTablesServiceProvider::class,
+
     ],
 
     /*
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
+        \App\Models\Karyawan::factory(10)->create([
+            'nama_karyawan' => 'Admin',
+            'nomor_rekening' => '1234567890',
+        ]);
     }
 }
